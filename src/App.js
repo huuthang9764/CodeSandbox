07126b009ec7d7a9bf06830216941d4533148da5
Nguyễn Hữu Thắng <76506824+huuthang9764@users.noreply.github.com>
2022-03-22 15:20:51 +0700

    1 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./styles.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox1</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
